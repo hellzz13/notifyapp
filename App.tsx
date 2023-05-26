82a74c8f3d4faeef65ed7da52fec21c5b3f043e6
hellzz13 <hpalenca13@gmail.com>
2023-05-26 14:22:04 -0300

    add notification

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
+
+import * as Notify from "expo-notifications";
+import { styles } from "./style";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    Notify.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldPlaySound: true,
+            shouldSetBadge: true,
+            shouldShowAlert: true,
+        }),
+    });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const handleCallNotification = async () => {
+        const { status } = await Notify.requestPermissionsAsync();
+
+        if (status !== "granted") {
+            Alert.alert("Você precisa ativar a permissão para notificações");
+        }
+
+        await Notify.scheduleNotificationAsync({
+            content: {
+                title: "Nova notificação",
+                body: "Agora funciona!",
+                color: "#000",
+            },
+            trigger: { seconds: 1 },
+        });
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textline}>
+                Esse app vai gerar uma notificação
+            </Text>
+            <StatusBar style="auto" />
+            <TouchableOpacity
+                style={styles.buttonNotification}
+                onPress={handleCallNotification}
+            >
+                <Text style={styles.textButton}>Clique aqui</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
---
+++ style.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textline: {
+        color: "#fff",
+        fontSize: 24,
+    },
+    textButton: {
+        color: "#fff",
+        fontSize: 18,
+    },
+    buttonNotification: {
+        marginTop: 20,
+        backgroundColor: "blue",
+        padding: 10,
+        borderRadius: 10,
+    },
+});
